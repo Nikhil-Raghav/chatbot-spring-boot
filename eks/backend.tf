3007ee86cdb926a918b99d7fc4f52055b77a9c66
Nikhil-Raghav <nikhilraghavvb20@gmail.com>
2026-02-17 14:34:04 +0530

    done

--- eks/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-state-1767419040"
-    key          = "java-dev/terraform.tfstate"
-    region       = "us-west-2"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
